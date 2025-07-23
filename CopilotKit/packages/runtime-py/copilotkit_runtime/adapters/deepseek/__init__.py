4a0bb31ae488411286a826bfd57da7aa4f512cba
chenchaotao666 <chenchaotao@huawei.com>
2025-07-23 10:32:56 +0800

    提交

--- CopilotKit/packages/runtime-py/copilotkit_runtime/adapters/deepseek/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-DeepSeek适配器
-
-对标TypeScript版本的DeepSeek适配器实现
-"""
-
-from .deepseek_adapter import DeepSeekAdapter, DeepSeekAdapterParams
-
-__all__ = ["DeepSeekAdapter", "DeepSeekAdapterParams"] 
