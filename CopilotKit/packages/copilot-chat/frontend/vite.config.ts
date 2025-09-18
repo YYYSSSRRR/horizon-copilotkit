742916efbede46e2454f736a0bbf0be44eaaec58
chenchaotao666 <chenchaotao@huawei.com>
2025-09-18 10:55:47 +0800

    删除没有使用到的内容

--- CopilotKit/packages/copilot-chat/frontend/vite.config.ts
+++ CopilotKit/packages/copilot-chat/frontend/vite.config.ts
@@ -12,14 +12,14 @@ export default defineConfig({
       '@copilotkit/shared': fileURLToPath(new URL('../../shared/src', import.meta.url)),
       '@copilotkit/playwright-actuator': fileURLToPath(new URL('../../playwright-actuator/src', import.meta.url)),
       // 具体路径映射需要在通用映射之前
-      'react/jsx-runtime': fileURLToPath(new URL('../node_modules/openinula/jsx-runtime.js', import.meta.url)),
-      'react/jsx-dev-runtime': fileURLToPath(new URL('../node_modules/openinula/jsx-dev-runtime.js', import.meta.url)),
-      'openinula/jsx-runtime': fileURLToPath(new URL('../node_modules/openinula/jsx-runtime.js', import.meta.url)),
-      'openinula/jsx-dev-runtime': fileURLToPath(new URL('../node_modules/openinula/jsx-dev-runtime.js', import.meta.url)),
+      'react/jsx-runtime': fileURLToPath(new URL('./node_modules/openinula/jsx-runtime.js', import.meta.url)),
+      'react/jsx-dev-runtime': fileURLToPath(new URL('./node_modules/openinula/jsx-dev-runtime.js', import.meta.url)),
+      'openinula/jsx-runtime': fileURLToPath(new URL('./node_modules/openinula/jsx-runtime.js', import.meta.url)),
+      'openinula/jsx-dev-runtime': fileURLToPath(new URL('./node_modules/openinula/jsx-dev-runtime.js', import.meta.url)),
       // 通用映射
-      'react': fileURLToPath(new URL('../node_modules/openinula/index.js', import.meta.url)),
-      'react-dom': fileURLToPath(new URL('../node_modules/openinula/index.js', import.meta.url)), 
-      'react-is': fileURLToPath(new URL('../node_modules/openinula/index.js', import.meta.url)),
+      'react': fileURLToPath(new URL('./node_modules/openinula/index.js', import.meta.url)),
+      'react-dom': fileURLToPath(new URL('./node_modules/openinula/index.js', import.meta.url)), 
+      'react-is': fileURLToPath(new URL('./node_modules/openinula/index.js', import.meta.url)),
     },
   },
   server: {
